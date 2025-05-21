a57b13d22d106523e0ac7f7e571a6cb82716a31c
CodeForgeNet <codeforgenet@icloud.com>
2025-05-21 11:02:45 +0530

    update postcss file

---
+++ postcss.config.mjs
@@ -0,0 +1,19 @@
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+
+// export default config;
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
